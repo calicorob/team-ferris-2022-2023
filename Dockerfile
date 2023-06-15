cb620431185be8c59f54f67eed84232a39a87c9d
calicorob <calicorob@users.noreply.github.com>
2023-06-14 20:11:50 -0400

    Add create boxscore table statement in start.sql

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,3 @@
 FROM jupyter/datascience-notebook:latest
 RUN pip install czapi 
+RUN pip install pg8000
--- sql_startup/start.sql
+++ sql_startup/start.sql
@@ -1,3 +1,22 @@
 CREATE DATABASE ferris;
 
+\c ferris;
+
 CREATE SCHEMA stg;
+
+
+CREATE TABLE stg.boxscore
+(
+     team_name TEXT
+    ,href TEXT
+    ,hammer_start BOOLEAN
+    ,score VARCHAR(2)[]
+    ,final_score VARCHAR(2)
+    ,draw_num SMALLINT
+    ,draw TEXT
+    ,hammer_progress BOOLEAN[]
+    ,relative_score SMALLINT[]
+    ,guid BIGINT
+
+
+);
