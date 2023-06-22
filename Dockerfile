9915b2479c0f91d227752b20399eeee99bbd5bb7
calicorob <calicorob@users.noreply.github.com>
2023-06-22 17:11:34 -0400

    Add db_startup.py

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM jupyter/datascience-notebook:latest
 RUN pip install czapi 
 RUN pip install pg8000
+RUN pip install sqlalchemy_utils
---
+++ work/config.py
@@ -0,0 +1,45 @@
+__all__ = ['db_engine','inspector', 'data_schema','boxscore_meta','boxscore_table']
+
+
+from sqlalchemy import Table, Column, String, ARRAY, Text, MetaData, Boolean, SmallInteger, BigInteger
+from sqlalchemy import URL, create_engine,text,schema
+from sqlalchemy import inspect
+
+
+# DB con
+url_object = URL.create(
+    "postgresql+pg8000",
+    username="postgres",
+    password="postgres",  # plain (unescaped) text
+    host="db",
+    database="ferris",
+)
+
+db_engine = create_engine(url_object)
+inspector = inspect(db_engine)
+
+# schema
+data_schema_name = 'data'
+data_schema = schema.CreateSchema(name=data_schema_name,if_not_exists=True)
+
+# tables
+boxscore_meta = MetaData()
+boxscore_table = Table(
+    'boxscore'
+    ,boxscore_meta
+    ,Column(name = 'team_name',type_=Text)
+    ,Column(name = 'href',type_ = Text)
+    ,Column(name='hammer_start',type_ = Boolean)
+    ,Column(name='score',type_=ARRAY(String(2)))
+    ,Column(name='final_score',type_ = String(2))
+    ,Column(name='draw_num',type_ = SmallInteger)
+    ,Column(name='draw',type_=Text)
+    ,Column(name='hammer_progress',type_=ARRAY(Boolean))
+    ,Column(name='relative_score',type_=ARRAY(SmallInteger))
+    ,Column(name='guid',type_=BigInteger)
+    ,schema=data_schema_name
+
+)
+
+if __name__ == '__main__':
+    pass
